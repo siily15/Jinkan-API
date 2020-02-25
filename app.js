3f6e5c3cfcddb56a135481a6d85ec915fabf087e
siily15 <55434210+siily15@users.noreply.github.com>
2020-02-25 10:48:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-fetch('https://api.jikan.moe/v3')
-    then(response=>{
-        return response.json()
+fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
+.then(response=>{
+    return response.json()
+
 }).then(data =>{
     console.log(data)
-
-    const anime = document.getElementById('anime')
-
-    data.forEach(anime => {
-        console.log(anime)
-        let animeDiv = document.createElement('div')
-        animeDiv.innerHTML = '<div class="title">' + anime.title  + '</div><div class=decription>' + anime.description + '</div>'
-        anime.append(animeDiv)
-    });
-
+    
 })
